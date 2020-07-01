cb419d540f8e4989b90d7271a1699b170b93525f
Benjamin Gordon <bmgordon@chromium.org>
2020-07-01 09:02:29 -0600

    CHROMIUM: Exclude D/ED from SC2115 in ebuilds
    $D and $ED are guaranteed to always be set, so patterns like
    
    rm -rf "${D}/var"
    
    can never expand to /var.  Make checkCatastrophicRm ignore these two
    variables when run on an ebuild.
    
    BUG=chromium:1026405
    TEST=shellcheck gentoolkit-0.3.0.8.ebuild
    
    Change-Id: Ie53dc27e7cd6c832ce0177187a3b4babdb3eaccd

--- src/ShellCheck/Checks/Commands.hs
+++ src/ShellCheck/Checks/Commands.hs
@@ -109,7 +109,7 @@ commandChecks params = [
     ,checkDeprecatedEgrep
     ,checkDeprecatedFgrep
     ,checkWhileGetoptsCase
-    ,checkCatastrophicRm
+    ,checkCatastrophicRm (isPortageBuild params)
     ,checkLetUsage
     ,checkMvArguments, checkCpArguments, checkLnArguments
     ,checkFindRedirections
@@ -925,20 +925,22 @@ checkWhileGetoptsCase = CommandCheck (Exactly "getopts") f
             T_Redirecting _ _ x@(T_CaseExpression {}) -> return x
             _ -> Nothing
 
-prop_checkCatastrophicRm1 = verify checkCatastrophicRm "rm -r $1/$2"
-prop_checkCatastrophicRm2 = verify checkCatastrophicRm "rm -r /home/$foo"
-prop_checkCatastrophicRm3 = verifyNot checkCatastrophicRm "rm -r /home/${USER:?}/*"
-prop_checkCatastrophicRm4 = verify checkCatastrophicRm "rm -fr /home/$(whoami)/*"
-prop_checkCatastrophicRm5 = verifyNot checkCatastrophicRm "rm -r /home/${USER:-thing}/*"
-prop_checkCatastrophicRm6 = verify checkCatastrophicRm "rm --recursive /etc/*$config*"
-prop_checkCatastrophicRm8 = verify checkCatastrophicRm "rm -rf /home"
-prop_checkCatastrophicRm10= verifyNot checkCatastrophicRm "rm -r \"${DIR}\"/{.gitignore,.gitattributes,ci}"
-prop_checkCatastrophicRm11= verify checkCatastrophicRm "rm -r /{bin,sbin}/$exec"
-prop_checkCatastrophicRm12= verify checkCatastrophicRm "rm -r /{{usr,},{bin,sbin}}/$exec"
-prop_checkCatastrophicRm13= verifyNot checkCatastrophicRm "rm -r /{{a,b},{c,d}}/$exec"
-prop_checkCatastrophicRmA = verify checkCatastrophicRm "rm -rf /usr /lib/nvidia-current/xorg/xorg"
-prop_checkCatastrophicRmB = verify checkCatastrophicRm "rm -rf \"$STEAMROOT/\"*"
-checkCatastrophicRm = CommandCheck (Basename "rm") $ \t ->
+prop_checkCatastrophicRm1 = verify (checkCatastrophicRm False) "rm -r $1/$2"
+prop_checkCatastrophicRm2 = verify (checkCatastrophicRm False) "rm -r /home/$foo"
+prop_checkCatastrophicRm3 = verifyNot (checkCatastrophicRm False) "rm -r /home/${USER:?}/*"
+prop_checkCatastrophicRm4 = verify (checkCatastrophicRm False) "rm -fr /home/$(whoami)/*"
+prop_checkCatastrophicRm5 = verifyNot (checkCatastrophicRm False) "rm -r /home/${USER:-thing}/*"
+prop_checkCatastrophicRm6 = verify (checkCatastrophicRm False) "rm --recursive /etc/*$config*"
+prop_checkCatastrophicRm8 = verify (checkCatastrophicRm False) "rm -rf /home"
+prop_checkCatastrophicRm10= verifyNot (checkCatastrophicRm False) "rm -r \"${DIR}\"/{.gitignore,.gitattributes,ci}"
+prop_checkCatastrophicRm11= verify (checkCatastrophicRm False) "rm -r /{bin,sbin}/$exec"
+prop_checkCatastrophicRm12= verify (checkCatastrophicRm False) "rm -r /{{usr,},{bin,sbin}}/$exec"
+prop_checkCatastrophicRm13= verifyNot (checkCatastrophicRm False) "rm -r /{{a,b},{c,d}}/$exec"
+prop_checkCatastrophicRmA = verify (checkCatastrophicRm False) "rm -rf /usr /lib/nvidia-current/xorg/xorg"
+prop_checkCatastrophicRmB = verify (checkCatastrophicRm False) "rm -rf \"$STEAMROOT/\"*"
+prop_checkCatastrophicRmED1 = verify (checkCatastrophicRm False) "rm -rf \"$ED/var/\"*"
+prop_checkCatastrophicRmED2 = verifyNot (checkCatastrophicRm True) "rm -rf \"$ED/var/\"*"
+checkCatastrophicRm isPortageBuild = CommandCheck (Basename "rm") $ \t ->
     when (isRecursive t) $
         mapM_ (mapM_ checkWord . braceExpand) $ arguments t
   where
@@ -968,7 +970,7 @@ checkCatastrophicRm = CommandCheck (Basename "rm") $ \t ->
         f (T_DollarBraced _ _ word) =
             let var = onlyLiteralString word in
                 -- This shouldn't handle non-colon cases.
-                if any (`isInfixOf` var) [":?", ":-", ":="]
+                if any (`isInfixOf` var) [":?", ":-", ":="] || (isPortageBuild && var `elem` ["D", "ED"])
                 then Nothing
                 else return ""
         f _ = return ""
